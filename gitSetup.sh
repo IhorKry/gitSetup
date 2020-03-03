43cf593b438cfca5f3dd10d6e17f12c98261d158
Igor Kryvopustov <ihorkr@tahometer.com>
2020-03-03 17:11:37 +0200

    part: create local user config for new project

--- gitSetup.sh
+++ gitSetup.sh
@@ -13,3 +13,21 @@ fi
 
 cd $pathToDir;
 git init;
+
+echo "Please, input user name for project. If you leave this fields empty - it will be written from global config";
+read userName;
+if [ ${#userName} -eq 0 ]
+then
+    git config --local user.name "$(git config --global user.name)";
+else
+    git config --local user.name $userName;
+fi
+
+echo "Please, input user email for project. If you leave this fields empty - it will be written from global config";
+read userEmail;
+if [ ${#userEmail} -eq 0 ]
+then
+    git config --local user.email $(git config --global user.email);
+else
+    git config --local user.email $userEmail;
+fi
