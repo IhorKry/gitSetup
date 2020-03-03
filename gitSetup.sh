8326c6a402666c1b5bca774996ce9a69c75c6e8f
Igor Kryvopustov <ihorkr@tahometer.com>
2020-03-03 17:58:13 +0200

    fix text in error description

--- gitSetup.sh
+++ gitSetup.sh
@@ -3,7 +3,7 @@
 FYEL="\033[33m" # foreground yellow
 pathToDir=$1;
 
-if [ ${#pathToDir} -eq 0 ]; then echo "You must put path for directory in format: ./createGitProject.sh [PATH TO DIR]"; exit 0; fi;
+if [ ${#pathToDir} -eq 0 ]; then echo "You must put path for directory in format: ./gitSetup.sh [PATH TO DIR]"; exit 0; fi;
 
 if [ ! -d $pathToDir ]
 then
